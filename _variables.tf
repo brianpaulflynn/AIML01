511176aff4463d8330668c58c1f9d69835a023bd
Brian Flynn <bflynn@battech.com>
2023-02-06 13:59:44 -0700

    initial setup

---
+++ _variables.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  type        = string
+  description = "Name of the deployment"
+  default     = "bpf7701"
+}
+
+variable "environment" {
+  type        = string
+  description = "Name of the environment"
+  default     = "dev"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of the resources"
+  #default     = "South Central US"
+  default     = "East US" 
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+    type = string
+}
+
+variable "ARM_TENANT_ID" {
+    type = string
+}
+
+variable "ARM_CLIENT_ID" {
+    type = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+    type = string
+}
