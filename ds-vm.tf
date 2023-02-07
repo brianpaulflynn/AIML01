921698865a3cebe0915ee86be46540bcd6ca9fef
Brian Flynn <bflynn@battech.com>
2023-02-06 19:37:05 -0700

    adding notes and cleaning up

--- ds-vm.tf
+++ ds-vm.tf
@@ -1,13 +1,7 @@
 # must run from cmd prompt: az vm image terms accept --urn microsoft-ads:linux-data-science-vm-ubuntu:linuxdsvmubuntu:latest
 # ref: https://github.com/anoff/tf-azure-datascience
-
 variable "vm-name" {
   default = "user1"
-}
-
-resource "azurerm_resource_group" "default" {
-  name     = "rg-${var.name}-${var.environment}"
-  location = var.location
 }
 
 resource "azurerm_resource_group" "ds" {
@@ -26,7 +20,7 @@ resource "azurerm_subnet" "ds" {
   name                 = "ds-subnet"
   resource_group_name  = "${azurerm_resource_group.ds.name}"
   virtual_network_name = "${azurerm_virtual_network.ds.name}"
-  address_prefix       = "10.0.2.0/24"
+  address_prefixes     = [ "10.0.2.0/24" ]
 }
 
 resource "azurerm_network_interface" "ds" {
